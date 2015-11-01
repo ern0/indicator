948e51236991244ee7760dcd5b12beb852248290
ern0 <ern0@linkbroker.hu>
2015-11-01 21:01:42 +0000

    test timing changed

--- ser.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-import serial
-import time
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.5)
-x = ":400c0005505c0333030a0;"
-for i in range(0,len(x)):
-	ser.write(x[i]);
-	time.sleep(0.01)
-ser.close()
--- test/ser.py
+++ test/ser.py
@@ -2,9 +2,9 @@
 import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.5)
-#x = ":400-004-400-004-400-004-400-004-400-400-004-400-400-004-400-004;+3:ff0;"
-x = ":+7:0f5;"
+x = ":400-004-400-004-400-004-400-004-400-400-004-400-400-004-400-004;+3:ff0;"
+#x = ":+7:0f5;"
 for i in range(0,len(x)):
 	ser.write(x[i]);
-	time.sleep(0.01)
+	time.sleep(0.11)
 ser.close()
