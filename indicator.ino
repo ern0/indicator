006c33f65929128c1a66992018e533d7197a24c7
ern0 <ern0@linkbroker.hu>
2016-01-22 14:58:08 +0100

    init flash added

--- indicator.ino
+++ indicator.ino
@@ -1,8 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 #include <DigiCDC.h>
 
+# define PIXELS 8
+# define PORT 2
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16,1,NEO_GRB | NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PORT, NEO_GRB | NEO_KHZ800);
 # define POS_NONE (255)
 unsigned char pos = POS_NONE;
 unsigned char colorIndex = 0;
@@ -16,103 +18,110 @@ unsigned char brite = 30;
 # define MOD_DATA (3)
 unsigned char mod = MOD_IDLE;
 
- 
+
 void setup() {
-  
+
   SerialUSB.begin();
   strip.begin();
-  strip.setBrightness(brite);
-  
-} // setup() 
+  strip.setBrightness(30);
+
+  for (int n = 0; n < PIXELS; n++) strip.setPixelColor(n,0,0,11);
+  strip.show();
+  delay(500);
+  for (int n = 0; n < PIXELS; n++) strip.setPixelColor(n,0,0,0);
+  strip.show();
+
+} // setup()
 
 
 void loop() {
-   
+
   unsigned char c = SerialUSB.read();
-    
+
   switch (c) {
 
     case '*': {
-      mod = MOD_BRITE;
-      brite = 0;
-      pos = POS_NONE;
-      posReset = true;
-    } break;
-    
+        mod = MOD_BRITE;
+        brite = 0;
+        pos = POS_NONE;
+        posReset = true;
+      } break;
+
     case '+': {
-      mod = MOD_POS;
-      pos = POS_NONE;
-      posReset = false;
-    } break;
+        mod = MOD_POS;
+        pos = POS_NONE;
+        posReset = false;
+      } break;
 
     case ':': {
-      mod = MOD_DATA;
-      if (posReset) pos = 0;
-      colorIndex = 0;
-    } break;
-    
+        mod = MOD_DATA;
+        if (posReset) pos = 0;
+        colorIndex = 0;
+      } break;
+
     case ';': {
-      mod = MOD_IDLE;
-      bool show = ( pos > 0 );
-      if (pos == POS_NONE) show = false;
-      if (show) strip.show();
-      pos = POS_NONE;
-      colorIndex = 0;  
-      posReset = true;      
-    } break;
-    
+        mod = MOD_IDLE;
+        bool show = ( pos > 0 );
+        if (pos == POS_NONE) show = false;
+        if (show) strip.show();
+        pos = POS_NONE;
+        colorIndex = 0;
+        posReset = true;
+      } break;
+
   } // switch char
 
   switch (mod) {
 
     case MOD_DATA: {
-      if (pos == POS_NONE) return;
-      
-      bool isDigit = false;    
-      if (('0' <= c) && (c <= '9')) {
-        isDigit = true;
-        c = c - '0';
-      }
-      if (('a' <= c) && (c <= 'f')) {
-        isDigit = true;
-        c = c - ('a' - 10);
-      }
-      if (('A' <= c) && (c <= 'F')) {
-        isDigit = true;
-        c = c - ('A' - 10);
-      }
-      if (!isDigit) return;
-    
-      color[colorIndex++] = c | (c << 4);
-      if (colorIndex < 3) return;
+        //if (pos == POS_NONE) return;
+        if (pos >= PIXELS) return;
 
-      if (brite == 0) brite = 30;
-      strip.setBrightness(brite);
-      strip.setPixelColor(pos,color[0],color[1],color[2]);
-      colorIndex = 0;
-      pos++;
+        bool isDigit = false;
+        if (('0' <= c) && (c <= '9')) {
+          isDigit = true;
+          c = c - '0';
+        }
+        if (('a' <= c) && (c <= 'f')) {
+          isDigit = true;
+          c = c - ('a' - 10);
+        }
+        if (('A' <= c) && (c <= 'F')) {
+          isDigit = true;
+          c = c - ('A' - 10);
+        }
+        if (!isDigit) return;
 
-    } break;
+        color[colorIndex++] = c | (c << 4);
+        if (colorIndex < 3) return;
+
+        if (brite == 0) brite = 30;
+        strip.setBrightness(brite);
+        strip.setPixelColor(pos, color[0], color[1], color[2]);
+        colorIndex = 0;
+        pos++;
+
+      } break;
 
     case MOD_POS: {
-      
-      if ((c < '0') || ('9' < c)) return;
-      if (pos == POS_NONE) pos = 0;
 
-      pos = 10 * pos;
-      pos += (c - '0');
-            
-    } break;
+        if ((c < '0') || ('9' < c)) return;
+        if (pos == POS_NONE) pos = 0;
+
+        pos = 10 * pos;
+        pos += (c - '0');
+
+      } break;
 
     case MOD_BRITE: {
 
-      if ((c < '0') || ('9' < c)) return;
+        if ((c < '0') || ('9' < c)) return;
 
-      brite = 10 * brite;
-      brite += (c - '0');
-      
-    } break;
-    
+        brite = 10 * brite;
+        brite += (c - '0');
+
+      } break;
+
   } // switch mod
-  
+
 } // loop()
