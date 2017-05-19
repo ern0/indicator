59f5985d9da62e51b8c7714f569cbf8d45d6e7f4
ern0 <ern0@linkbroker.hu>
2017-05-19 18:30:17 +0200

    client tabs work

--- client/script.js
+++ client/script.js
@@ -1,6 +1,17 @@
 	$(document).ready(function() {
 		
 		activateTab(1);
+
+		$(".tabsel").click(function() {
+			var id = $(this).attr("id");
+			$(this).addClass(id + "active");
+			setTimeout(function() {
+				$("#" + id).removeClass(id + "active");
+			},200)
+
+			var tabnum = id.substr(6);
+			activateTab(tabnum);
+		})
 		
 	})
 
