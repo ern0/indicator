5e00dc33c5e9599d12b79a7e8546ef9cc7e063d1
ern0 <ern0@linkbroker.hu>
2019-11-16 11:57:50 +0100

    reformatted and split to functions

--- firmware/src/indicator.cpp
+++ firmware/src/indicator.cpp
@@ -1,140 +1,148 @@
-# include "indicator.hpp"
 #include <Adafruit_NeoPixel.h>
+#include "indicator.hpp"
 
-///#include <DigiCDC.h>
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB | NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN_NEOPIX, NEO_GRB | NEO_KHZ800);
 
 unsigned char pos = POS_NONE;
 unsigned char colorIndex = 0;
 unsigned char color[3];
 bool posReset = true;
 unsigned char brite = 30;
-
 unsigned char mod = MOD_IDLE;
-
-
-void clear(int r,int g,int b) {
-
-  for (int n = 0; n < PIXELS; n++) strip.setPixelColor(n,r,g,b);
-  strip.show();
-
-  pos = POS_NONE;
-
-} // clear()
+unsigned char c;
 
 
 void setup() {
 
-  Serial.begin(9600);
-  strip.begin();
-  strip.setBrightness(30);
+	Serial.begin(9600);
+	strip.begin();
+	strip.setBrightness(30);
 
-  clear(0xff,0xff,0xff);
+	clear(0xff,0xff,0xff);
 
 } // setup()
 
 
 void loop() {
 
-  unsigned char c = Serial.read();
-  
-  switch (c) {
+	c = Serial.read();
 
-    case '!': {
-        clear(0,0,0);        
-        return;  
-    }
-
-
-    case '?': {
-      Serial.write(SIGNATURE);
-      return; 
-    }
-
-    
-    case '*': {
-        mod = MOD_BRITE;
-        brite = 0;
-        pos = POS_NONE;
-        posReset = true;
-      } break;
-
-    case '+': {
-        mod = MOD_POS;
-        pos = POS_NONE;
-        posReset = false;
-      } break;
-
-    case ':': {
-        mod = MOD_DATA;
-        if (posReset) pos = 0;
-        colorIndex = 0;
-      } break;
-
-    case ';': {
-        mod = MOD_IDLE;
-        bool show = ( pos > 0 );
-        if (pos == POS_NONE) show = false;
-        if (show) strip.show();
-        pos = POS_NONE;
-        colorIndex = 0;
-        posReset = true;
-      } break;
-
-  } // switch char
-
-  switch (mod) {
-
-    case MOD_DATA: {
-
-        if (pos >= PIXELS) return;
-
-        bool isDigit = false;
-        if (('0' <= c) && (c <= '9')) {
-          isDigit = true;
-          c = c - '0';
-        }
-        if (('a' <= c) && (c <= 'f')) {
-          isDigit = true;
-          c = c - ('a' - 10);
-        }
-        if (('A' <= c) && (c <= 'F')) {
-          isDigit = true;
-          c = c - ('A' - 10);
-        }
-        if (!isDigit) return;
-
-        color[colorIndex++] = c | (c << 4);
-        if (colorIndex < 3) return;
-
-        if (brite == 0) brite = 30;
-        strip.setBrightness(brite);
-        strip.setPixelColor(pos, color[0], color[1], color[2]);
-        colorIndex = 0;
-        pos++;
-
-      } break;
-
-    case MOD_POS: {
-
-        if ((c < '0') || ('9' < c)) return;
-        if (pos == POS_NONE) pos = 0;
-
-        pos = 10 * pos;
-        pos += (c - '0');
-
-      } break;
-
-    case MOD_BRITE: {
-
-        if ((c < '0') || ('9' < c)) return;
-
-        brite = 10 * brite;
-        brite += (c - '0');
-
-      } break;
-
-  } // switch mod
+	procChar();
+	procMode();
 
 } // loop()
+
+
+bool procChar() {
+
+	switch (c) {
+
+		case '!':
+			clear(0,0,0);
+			return true;
+
+		case '?':
+			Serial.write(SIGNATURE);
+			return true;
+
+		case '*':
+			mod = MOD_BRITE;
+			brite = 0;
+			pos = POS_NONE;
+			posReset = true;
+			break;
+
+		case '+':
+			mod = MOD_POS;
+			pos = POS_NONE;
+			posReset = false;
+			break;
+
+		case ':':
+			mod = MOD_DATA;
+			if (posReset) pos = 0;
+			colorIndex = 0;
+			break;
+
+		case ';':
+			mod = MOD_IDLE;
+			bool show = ( pos > 0 );
+			if (pos == POS_NONE) show = false;
+			if (show) strip.show();
+			pos = POS_NONE;
+			colorIndex = 0;
+			posReset = true;
+			break;
+
+	} // switch char
+
+	return false;
+} // procChar()
+
+
+void procMode() {
+
+	switch (mod) {
+
+		case MOD_DATA:
+
+			if (pos >= PIXELS) return;
+
+			bool isDigit = false;
+			if (('0' <= c) && (c <= '9')) {
+				isDigit = true;
+				c = c - '0';
+			}
+			if (('a' <= c) && (c <= 'f')) {
+				isDigit = true;
+				c = c - ('a' - 10);
+			}
+			if (('A' <= c) && (c <= 'F')) {
+				isDigit = true;
+				c = c - ('A' - 10);
+			}
+			if (!isDigit) return;
+
+			color[colorIndex++] = c | (c << 4);
+			if (colorIndex < 3) return;
+
+			if (brite == 0) brite = 30;
+			strip.setBrightness(brite);
+			strip.setPixelColor(pos, color[0], color[1], color[2]);
+			colorIndex = 0;
+			pos++;
+
+			break;
+
+		case MOD_POS:
+
+			if ((c < '0') || ('9' < c)) return;
+			if (pos == POS_NONE) pos = 0;
+
+			pos = 10 * pos;
+			pos += (c - '0');
+
+			break;
+
+		case MOD_BRITE:
+
+			if ((c < '0') || ('9' < c)) return;
+
+			brite = 10 * brite;
+			brite += (c - '0');
+
+			break;
+
+	} // switch mod
+
+} // procMode()
+
+
+void clear(int r,int g,int b) {
+
+	for (int n = 0; n < PIXELS; n++) strip.setPixelColor(n,r,g,b);
+	strip.show();
+
+	pos = POS_NONE;
+
+} // clear()
--- firmware/src/indicator.hpp
+++ firmware/src/indicator.hpp
@@ -1,9 +1,15 @@
 # ifndef indicator_hpp
 # define indicator_hpp
 
-# define PIXELS 128
-# define PIN 13
 # define SIGNATURE "lite\n"
+
+# define PIXELS 8
+# define PIN_NEOPIX 2
+
+# define PIN_BUTTON1 8
+# define PIN_BUTTON2 12
+# define PIN_GND1 6
+# define PIN_GND2 10s
 
 # define POS_NONE (255)
 
@@ -11,5 +17,9 @@
 # define MOD_BRITE (1)
 # define MOD_POS (2)
 # define MOD_DATA (3)
+
+inline bool procChar();
+inline void procMode();
+void clear(int r,int g,int b);
 
 # endif
